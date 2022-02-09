4eb9a73422e2415b7457dae89c0e86a5c9ae3649
KIIRIKOU <konteyemouhamadou@gmail.com>
2022-02-09 13:42:31 +0000

    No route found for GET

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -5,12 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Produit;
 
 class ProduitController extends AbstractController
 {
     #[Route('/Produit/liste', name: 'produit_liste')]
     public function index(): Response
     {
+        return $this->render('produit/liste.html.twig');
+    }
+
+    // #[Route('/Produit/get/{$id}', name: 'produit_get')]
+    // public function getProduit($id): Response
+    // {
+    //     return $this->render('produit/liste.html.twig');
+    // }
+
+    #[Route('/Produit/add', name: 'produit_liste')]
+    public function add()
+    {
+        $p = new Produit();
+        $p->setLibelle(libelle: "Scanner");
+        $p->setQtStock(qtStock:0.0);
+
+        $em = $this->getManager();
+        $em->persist($p);
+        $em->flush();
         return $this->render('produit/liste.html.twig');
     }
 }
